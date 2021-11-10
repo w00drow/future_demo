80263d84103504e27b9340c671093df2ced64a76
Dmitriy Matveichev <dmitriy.matveichev@qligent.com>
2021-11-10 10:26:31 +0300

    Use poll in server

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <chrono>
 #include <Poco/Net/DatagramSocket.h>
 #include <Poco/Net/SocketAddress.h>
 
@@ -46,21 +47,28 @@ int main(int argc, char** argv)
 
     while (true)
     {
-        char buffer[1024];
-        Poco::Net::SocketAddress client_address;
-        auto size = udp_socket.receiveFrom(buffer, sizeof(buffer) - 1, client_address);
-        buffer[size] = '\0';
+        if (udp_socket.poll(Poco::Timespan(1, 0), Poco::Net::Socket::SelectMode::SELECT_READ))
+        {
+            char buffer[1024];
+            Poco::Net::SocketAddress client_address;
+            auto size = udp_socket.receiveFrom(buffer, sizeof(buffer) - 1, client_address);
+            buffer[size] = '\0';
 
-        std::cout << "Received \"" << buffer << "\" from the client " << client_address.toString() << std::endl;
+            std::cout << "Received \"" << buffer << "\" from the client " << client_address.toString() << std::endl;
 
-        std::cout << "Calculating the result ... " << std::endl;
+            std::cout << "Calculating the result ... " << std::endl;
 
-        auto sum = getSumOfAllPrimesBelow(std::atol(buffer));
+            auto sum = getSumOfAllPrimesBelow(std::atol(buffer));
 
-        std::cout << "Sending result \"" << sum << "\" back..." << std::endl;
+            std::cout << "Sending result \"" << sum << "\" back..." << std::endl;
 
-        auto sum_string = std::to_string(sum);
-        udp_socket.sendTo(sum_string.c_str(), sum_string.size(), client_address);
+            auto sum_string = std::to_string(sum);
+            udp_socket.sendTo(sum_string.c_str(), sum_string.size(), client_address);
+        }
+        else
+        {
+            std::cout << "Doing service actions..." << std::endl;
+        }
     }
 
     return 0;
